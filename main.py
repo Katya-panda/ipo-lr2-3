37ba7e2144d82657a46f8909e2ba7ecee08536bc
Katya <katya_7002@mail.ru>
2024-09-29 19:14:19 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
 import math
-def calculate_radius(volume):
-    radius = ((3 * volume) / (4 * math.pi)) ** (1 / 3)
-    return radius
-x = float(input("Введите объём шара в куб. ед.: "))
-radius = calculate_radius(x)
-print(f"Исковый радиус шара: {radius:.5f}")
+v= float(input("Введите обьём шара в куб. ед.: "))
+r = ((3 * v) / (4 * math.pi)) ** (1 / 3)
+print("Радиус шара: ", r)
